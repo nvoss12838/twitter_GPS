fc45af9b7a440b4ded47198f65f198200c497582
Nick Voss <nvoss@mail.usf.edu>
2017-08-16 19:39:23 -0400

    first commit

---
+++ twitter_gps.R
@@ -0,0 +1,14 @@
+load.gps <- function(filname) {
+  #load GPS datafile return dataframe in time sereis format
+  df <- read.table(filename)
+  
+}
+
+dec.2.timestamp <- function(ts){
+  #convert decimalyears to timestamp in dataframe
+}
+
+#run twitter anomoly
+
+#subtract points
+
