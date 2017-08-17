bd2f01e9b10b43986dad2296716ba090746d2466
Nick Voss <nvoss@mail.usf.edu>
2017-08-17 09:20:35 -0400

    add write option and remove outliers function

--- twitter_gps.R
+++ twitter_gps.R
@@ -1,14 +1,30 @@
-load.gps <- function(filname) {
+load.gps <- function(filname){
   #load GPS datafile return dataframe in time sereis format
-  df <- read.table(filename)
-  
+  numCols <- max(count.fields(filname))
+  df <- read.table(filename,colClasses = c(rep("integer", 2), rep("NULL", numCols-2)))
+  return (df)
 }
 
 dec.2.timestamp <- function(ts){
   #convert decimalyears to timestamp in dataframe
+  h<-c()
+  for (data in c(ts[1])){h<-append(h,date_decimal(data))}
+  ts[1]<-h
+  return (ts) 
 }
 
 #run twitter anomoly
-
+run.twitter <- function(ts){
+  #
+}
 #subtract points
+remove.Outliers <- function(ts,outlier){
+ ts2 <-ts[ ! ts %in% outlier, ]
+ return (ts2)
+}
+
+new.TS.2.file <- function(ts,fname){
+  #write outlier less file to new file 
+  write.table(ts,file=fname)
+}
 
